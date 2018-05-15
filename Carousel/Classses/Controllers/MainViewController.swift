de31ce8cefc18d2a5bf702b9ba67d80092ba1d48
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-05-16 07:04:17 +0900

    初期設定を追加

---
+++ Carousel/Classses/Controllers/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainViewController.swift
+//  Carousel
+//
+//  Created by Miyoshi Masataka on 2018/05/16.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
